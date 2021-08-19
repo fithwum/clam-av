4302072f7d484966aa813d5beb7cb1ca561ef97b
fithwum <34775371+fithwum@users.noreply.github.com>
2021-08-19 06:57:26 -0700

    Create Install_Script.sh

---
+++ files/Install_Script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) 2018 fithwum
+# All rights reserved
+
+# Variables.
+
+
+# Main.
+# Check for DB files in /scan and update/download if needed.
+
+
+# Set permissions.
+chown 99:100 -R /scan
+chmod 776 -R /scan
+
+# Run.
+
+
+exit
